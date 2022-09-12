eaa88b6168ece51b1ee5d1f6915626fb9e06b5d7
Gervase <g.tuxworth@griffith.edu.au>
2022-09-12 13:31:55 +1000

    Added more plots

--- climate.py
+++ climate.py
@@ -11,5 +11,10 @@ plt.title("Climate Data")
 plt.ylabel("[CO2]") 
 plt.xlabel("Year (decade)") 
 
-
+plt.subplot(2, 1, 2)
+plt.plot(years, temp, 'r*-') 
+plt.ylabel("Temp (C)") 
+plt.xlabel("Year (decade)") 
+plt.show() 
+plt.savefig("co2_temp.png") 
 
